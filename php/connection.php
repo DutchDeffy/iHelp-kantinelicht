0717d374fcdae0aa59dc8c328500aae1d739d616
DutchDeffy <jannick.bloemendal@gmail.com>
2018-02-14 21:55:36 +0100

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -3,15 +3,23 @@
  
 function Connect()
 {
- $dbhost = "IP moet ik nog weten";
- $dbuser = "root";
- $dbpass = "Kantine";
- $dbname = "votes";
+ $dbhost = "10.100.137.78";
+ $dbuser = "ihelp";
+ $dbpass = "WACHTWOORDHIER";
+ $dbname = "ihelpdmx";
  
  // Create connection
  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
  
- return $conn;
+if (!$conn) {
+    die("Connectie met MySQL gefaald: " . mysqli_connect_error());
 }
+
+$sql = "CREATE TABLE stemkleuren (
+ _key       INT         NOT NULL AUTO_INCREMENT PRIMARY KEY
+, kleur      VARCHAR(10) NOT NULL DEFAULT ''
+, ip_address VARCHAR(16) NOT NULL DEFAULT 'unknown'
+, when_voted TIMESTAMP
+)";
  
 ?>
