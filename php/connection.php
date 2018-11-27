6c15919475f442620d8d37bcd31957a6f34a18dd
Jannick <jannick.bloemendal@gmail.com>
2018-11-27 10:19:36 +0100

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,62 +1,46 @@
-<?php // RAY_EE_voting_create.php
-/**
- * https://www.experts-exchange.com/articles/5256/Simple-Vote-Counting-in-PHP-and-MySQL.html
- * Demonstrate the voting algorithm -- Create the Tables
- */
-error_reporting(E_ALL);
-// CONNECTION AND SELECTION VARIABLES FOR THE DATABASE
-$db_host = "localhost"; // PROBABLY THIS IS OK
-$db_name = "ihelpdmx";        // GET THESE FROM YOUR HOSTING COMPANY
-$db_user = "ihelp";
-$db_word = "WACHTWOORD";
-// OPEN A CONNECTION TO THE DATA BASE SERVER AND SELECT THE DB
-$mysqli = new mysqli($db_host, $db_user, $db_word, $db_name);
-// DID THE CONNECT/SELECT WORK OR FAIL?
-if ($mysqli->connect_errno)
-{
-    $err
-    = "CONNECT FAIL: "
-    . $mysqli->connect_errno
-    . ' '
-    . $mysqli->connect_error
-    ;
-    trigger_error($err, E_USER_ERROR);
-}
-// IF WE GOT THIS FAR WE CAN DO QUERIES
-// THESE LINES REMOVE EXISTING VERSIONS OF THE TABLES
-$mysqli->query("DROP TABLE EE_vote_colors");
-$mysqli->query("DROP TABLE EE_vote_votes");
-// CREATE THE COLOR TABLE
-$sql
-=
-"
-CREATE TABLE EE_vote_colors
-( _key  INT         NOT NULL AUTO_INCREMENT PRIMARY KEY
-, color VARCHAR(10) NOT NULL DEFAULT ''
-)
-"
-;
-$res = $mysqli->query($sql) or trigger_error( "$sql<br/>" . $mysqli->error, E_USER_WARNING );
-// CREATE THE VOTES TABLE
-$sql
-=
-"
-CREATE TABLE EE_vote_votes
-( _key       INT         NOT NULL AUTO_INCREMENT PRIMARY KEY
-, color      VARCHAR(10) NOT NULL DEFAULT ''
-, ip_address VARCHAR(16) NOT NULL DEFAULT 'unknown'
-, when_voted TIMESTAMP
-)
-"
-;
-$res = $mysqli->query($sql) or trigger_error( "$sql<br/>" . $mysqli->error, E_USER_WARNING );
-// LOAD THE STANDARD ROY-G-BIV DATA INTO THE COLOR TABLE
-$res = $mysqli->query( "INSERT INTO stemkleuren ( kleur ) VALUES ( 'red'    )" );
-$res = $mysqli->query( "INSERT INTO stemkleuren ( kleur ) VALUES ( 'orange' )" );
-$res = $mysqli->query( "INSERT INTO stemkleuren ( kleur ) VALUES ( 'yellow' )" );
-$res = $mysqli->query( "INSERT INTO stemkleuren ( kleur ) VALUES ( 'green'  )" );
-$res = $mysqli->query( "INSERT INTO stemkleuren ( kleur ) VALUES ( 'lgreen' )" );
-$res = $mysqli->query( "INSERT INTO stemkleuren ( kleur ) VALUES ( 'blue'   )" );
-$res = $mysqli->query( "INSERT INTO stemkleuren ( kleur ) VALUES ( 'purple' )" );
-$res = $mysqli->query( "INSERT INTO stemkleuren ( kleur ) VALUES ( 'pink'   )" );
-$res = $mysqli->query( "INSERT INTO stemkleuren ( kleur ) VALUES ( 'clz'    )" );
+<?php
+require_once('php/credentials.php')
+
+$PDO = new PDO('mysql:dbname='.$dbname.';host=localhost', $dbuser, $dbpass);
+$query = $PDO->prepare('UPDATE kleuren SET votes = votes + 1 WHERE kleur = :kleur');
+
+$result = $query->execute(array(':kleur' => $_POST['color']));
+?>
+
+echo "<!DOCTYPE html>"
+echo "<html lang='nl'>"
+echo ""
+echo "<head>"
+echo "    <title>iHelp - KantineLED"
+echo "    </title>"
+echo "    <meta charset='UTF-8'>"
+echo "    <meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+echo "    <link rel='stylesheet' href='../css/style.css'>"
+echo "</head>"
+echo ""
+echo "<body>"
+echo "    <header>"
+echo "        <h1 id='title'>Bedankt voor het stemmen!</h1>"
+echo "    </header>"
+echo "    <article>"
+echo "            <p id='timerTitle'>Volgende kleur over:</p>"
+echo "        <div id='timerContainer'>"
+echo "            <p id='time'></p>"
+echo "        </div>"
+echo "    </article>"
+echo "    <script>
+        setInterval(function () {
+            var now = new Date();
+            var minutesN = now.getMinutes();
+            var secondsN = now.getSeconds();
+            var nextTime = 4 - minutesN % 5;
+            var minutes = nextTime;
+            var seconds = 59 - secondsN;
+            if (seconds < 10) { seconds = '0' + seconds; };
+            now = minutes + ':' + seconds;
+            document.getElementById('time').innerHTML = '<br>' + now;
+        }, 1000)
+</script>"
+echo "</body>"
+echo ""
+echo "</html>"
